0f88b63b901f5e3bd4dad50b347d49c22f6b1f5d
konrad <kon_ole@wp.pl>
2016-09-02 14:51:20 +0200

    Removed folder from repository

--- dist/out-tsc/app/environments/environment.dev.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: false
-};
-//# sourceMappingURL=E:/Programowanie/front-end/projects/angular-tutorial/src/app/environments/environment.dev.js.map
--- dist/out-tsc/app/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=E:/Programowanie/front-end/projects/angular-tutorial/src/app/environments/environment.prod.js.map
