9371549178529057152a498030f8187cf7e4e745
konrad <kon_ole@wp.pl>
2016-09-04 18:40:30 +0200

    Revert "Update angular to RC 6 cd ..."
    This reverts commit d3e3e1985535e41d7f42cd50d4772160cada92e2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,7 @@ export const firebaseConfig = {
 
 @NgModule({
   bootstrap: [App],
-  declarations: [App,ContactListComponent,ContactViewComponent, ContactFormComponent,Home,PictureDetailComponent,About],
+  declarations: [App,ContactListComponent,ContactViewComponent, ContactFormComponent,Home,PictureDetailComponent],
   imports: [ // An array of modules that you want to others in your application such as the forms or http modules
     AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
@@ -55,7 +55,7 @@ export const firebaseConfig = {
     ReactiveFormsModule,
   ],
   providers: [ContactService,PictureService] ,
-  exports:      [  ContactListComponent,ContactViewComponent, ContactFormComponent, Home,PictureDetailComponent,About,
+  exports:      [  ContactListComponent,ContactViewComponent, ContactFormComponent, Home,PictureDetailComponent,
     RouterModule,
     HttpModule,
     FormsModule,
