39e2265d4c0a13029b85036d50b609cc441d8031
konrad <kon_ole@wp.pl>
2016-09-14 12:23:49 +0200

    cleaning

--- node_modules/angularfire2/database/firebase_object_factory.d.ts
+++
@@ -1,4 +0,0 @@
-import { FirebaseObjectObservable } from './index';
-import { FirebaseObjectFactoryOpts } from '../interfaces';
-import 'rxjs/add/operator/mergeMap';
-export declare function FirebaseObjectFactory(absoluteUrlOrDbRef: string | firebase.database.Reference, {preserveSnapshot, query}?: FirebaseObjectFactoryOpts): FirebaseObjectObservable<any>;
