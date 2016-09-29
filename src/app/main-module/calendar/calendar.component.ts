f957facb8e7f1f37d58a4ca01636565b6e13eeb0
konrad <kon_ole@wp.pl>
2016-09-29 15:56:39 +0200

    reverse connection

--- src/app/main-module/calendar/calendar.component.ts
+++ src/app/main-module/calendar/calendar.component.ts
@@ -9,12 +9,12 @@ import { SelectItem } from '../../../assets/primeng/components/common/api';
 })
 export class CalendarComponent implements OnInit {
   header: any;
-  dialogVisible: boolean = false;
-  contactDetailsVisible: boolean = false;
-  addContactDialogVisible: boolean = false;
-  calendarContact: SimplyCalendarModel;
-  showContact: string = 'Pick Contact';
-  selectedContact: SelectItem;
+  // dialogVisible: boolean = false;
+  // contactDetailsVisible: boolean = false;
+  // addContactDialogVisible: boolean = false;
+  // calendarContact: SimplyCalendarModel;
+  // showContact: string = 'Pick Contact';
+  // selectedContact: SelectItem;
 
   constructor(private calendarService: CalendarService,
               private cd: ChangeDetectorRef,
@@ -22,9 +22,9 @@ export class CalendarComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.calendarService.getCalendarContacts();
-    this.contactService.getMappedContacts();
-    this.calendarContact = new SimplyCalendarModel();
+    // this.calendarService.getCalendarContacts();
+    // this.contactService.getMappedContacts();
+    // this.calendarContact = new SimplyCalendarModel();
     this.header = {
       left: 'prev,next today',
       center: 'title',
@@ -33,65 +33,65 @@ export class CalendarComponent implements OnInit {
   }
 
   handleDayClick(e) {
-    this.calendarContact = new SimplyCalendarModel();
-    this.calendarContact.start = e.date.format();
-    this.calendarContact.end = e.date.format();
-    this.contactDetailsVisible = false;
-    this.dialogVisible = true;
+    // this.calendarContact = new SimplyCalendarModel();
+    // this.calendarContact.start = e.date.format();
+    // this.calendarContact.end = e.date.format();
+    // this.contactDetailsVisible = false;
+    // this.dialogVisible = true;
     this.cd.detectChanges();
   }
 
   handleEventClick(e) {
-    this.calendarContact = new SimplyCalendarModel();
-    this.contactDetailsVisible = true;
-    let start = e.calEvent.start;
-    let end = e.calEvent.end;
-    if (e.view.name === 'month') {
-      start.stripTime();
-    }
-    if (end) {
-      end.stripTime();
-      this.calendarContact.end = end.format();
-    } else {
-      this.calendarContact.end = start.format();
-    }
-    this.calendarContact.$key = e.calEvent.$key;
-    this.calendarContact.parentKey = e.calEvent.parentKey;
-    this.calendarContact.title = e.calEvent.title;
-    this.calendarContact.start = start.format();
-    this.dialogVisible = true;
+    // this.calendarContact = new SimplyCalendarModel();
+    // this.contactDetailsVisible = true;
+    // let start = e.calEvent.start;
+    // let end = e.calEvent.end;
+    // if (e.view.name === 'month') {
+    //   start.stripTime();
+    // }
+    // if (end) {
+    //   end.stripTime();
+    //   this.calendarContact.end = end.format();
+    // } else {
+    //   this.calendarContact.end = start.format();
+    // }
+    // this.calendarContact.$key = e.calEvent.$key;
+    // this.calendarContact.parentKey = e.calEvent.parentKey;
+    // this.calendarContact.title = e.calEvent.title;
+    // this.calendarContact.start = start.format();
+    // this.dialogVisible = true;
   }
 
-  editContact() {
-    this.calendarService.updateCalendarContact(this.calendarContact);
-    this.calendarService.getCalendarContacts();
-    this.dialogVisible = false;
-  }
+  // editContact() {
+  //   this.calendarService.updateCalendarContact(this.calendarContact);
+  //   this.calendarService.getCalendarContacts();
+  //   this.dialogVisible = false;
+  // }
 
-  deleteContact() {
-    this.dialogVisible = false;
-    this.calendarService.deleteCalendarContact(this.calendarContact.$key);
-  }
+  // deleteContact() {
+  //   this.dialogVisible = false;
+  //   this.calendarService.deleteCalendarContact(this.calendarContact.$key);
+  // }
 
-  showAddContactDialog() {
-    this.dialogVisible = false;
-    this.addContactDialogVisible = true;
-  }
+  // showAddContactDialog() {
+  //   this.dialogVisible = false;
+  //   this.addContactDialogVisible = true;
+  // }
 
-  selectContact(contact: SelectItem) {
-    this.selectedContact = contact;
-    this.showContact = contact.value;
-  }
+  // selectContact(contact: SelectItem) {
+  //   this.selectedContact = contact;
+  //   this.showContact = contact.value;
+  // }
 
-  send() {
-    let model = new SimplyCalendarModel();
-    model.parentKey = this.selectedContact.label;
-    model.title = this.selectedContact.value;
-    model.end = this.calendarContact.end;
-    model.start = this.calendarContact.start;
-    this.calendarService.insertCalendarContact(model);
-    this.addContactDialogVisible = false;
-    this.dialogVisible = false;
-    this.contactDetailsVisible = false;
-  }
+  // send() {
+  //   let model = new SimplyCalendarModel();
+  //   model.parentKey = this.selectedContact.label;
+  //   model.title = this.selectedContact.value;
+  //   model.end = this.calendarContact.end;
+  //   model.start = this.calendarContact.start;
+  //   this.calendarService.insertCalendarContact(model);
+  //   this.addContactDialogVisible = false;
+  //   this.dialogVisible = false;
+  //   this.contactDetailsVisible = false;
+  // }
 }
